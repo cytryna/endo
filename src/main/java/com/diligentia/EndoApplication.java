41ff197fca0984b45f26f65ab70b95ebe223cd09
rwichrowski <rwichrowski@efigence.com>
2017-04-03 14:47:20 +0200

    Initial commit

---
+++ src/main/java/com/diligentia/EndoApplication.java
@@ -0,0 +1,19 @@
+package com.diligentia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndoApplication.class, args);
+
+
+		for (int i = 0; i < 100; i++) {
+			System.err.println("ssssssssss");
+		}
+
+
+	}
+}
