70d1535d6f9e7c720a82862b017851d6d1250465
radek <radoslaw.wichrowski@gmail.com>
2017-04-29 06:35:11 +0200

    Połączenie i sparsownia rywalizacji

--- src/main/java/com/diligentia/DownloadPage.java
+++ src/main/java/com/diligentia/DownloadPage.java
@@ -4,16 +4,10 @@ import com.diligentia.model.SessionLoginRequest;
 import com.google.gson.Gson;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Map;
 
 @Component
@@ -21,107 +15,64 @@ public class DownloadPage {
 
     public static final String HTTP_URL_HOME = "https://www.endomondo.com/home";
     public static final String HTTP_URL_SESSION = "https://www.endomondo.com/rest/session";
-    public static final String HTTP_URL_CHALLENGE = "https://www.endomondo.com/challenges/32422477";
-//	public static final String HTTP_URL_CHALLENGE = "https://www.endomondo.com/rest/v1/challenges/32422477";
+    public static final String HTTP_URL_CHALLENGE = "https://www.endomondo.com/challenges";
 	private SessionLoginRequest sessionLoginRequest = new SessionLoginRequest("radoslaw.wichrowski@gmail.com", "", true);
+	private Map<String, String> cookiesSession;
+	private Map<String, String> cookiesHome;
 
-    @PostConstruct
+	@PostConstruct
 	public void startBean() {
-		Map<String, String> cookies = null;
 		try {
-			cookies = conectAndGetSession();
+			conectAndGetSession();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		try {
-			dowloadAndParse(HTTP_URL_CHALLENGE, cookies);
+			dowloadAndParseChallenge("/32422477");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-//		for (int i = 0; i < 10; i++) {
-//			try {
-//				dowloadAndParse(HTTP_URL_CHALLENGES);
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//			try {
-//				Thread.sleep(3000);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//		}
-
 	}
 
-    private Map<String, String> conectAndGetSession() throws IOException {
+    private void conectAndGetSession() throws IOException {
 		Gson gson = new Gson();
 		Connection.Response res = Jsoup
 				.connect(HTTP_URL_HOME)
 				.userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0")
 				.method(Connection.Method.GET)
 				.execute();
-		Map<String, String> cookies = res.cookies();
+		cookiesHome = res.cookies();
 
-//		System.err.println(cookies.get("CSRF_TOKEN"));
-
-
-		//This will get you the response.
-		System.err.println(gson.toJson(sessionLoginRequest));
 		Connection.Response res2 = Jsoup
                 .connect(HTTP_URL_SESSION)
 				.requestBody(gson.toJson(sessionLoginRequest))
-				.header("X-CSRF-TOKEN", cookies.get("CSRF_TOKEN"))
+				.header("X-CSRF-TOKEN", cookiesHome.get("CSRF_TOKEN"))
 				.header("Content-Type", "application/json;charset=utf-8")
 				.header("Accept", "application/json, text/plain, */*")
 				.header("Accept-Language", "en-US,en;q=0.5")
 				.userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0")
-//				.proxy("127.0.0.1", 8080)
-//				.validateTLSCertificates(false)
-				.cookies(cookies)
+				.cookies(cookiesHome)
 				.ignoreContentType(true)
 				.method(Connection.Method.POST)
                 .execute();
+		cookiesSession = res2.cookies();
 
+    }
 
+    public void dowloadAndParseChallenge(String challengeId) throws IOException {
 		Connection.Response res3 = Jsoup
-				.connect(HTTP_URL_CHALLENGE)
-//				.requestBody(gson.toJson(sessionLoginRequest))
-				.header("X-CSRF-TOKEN", cookies.get("CSRF_TOKEN"))
+				.connect(HTTP_URL_CHALLENGE + challengeId)
+				.header("X-CSRF-TOKEN", cookiesHome.get("CSRF_TOKEN"))
 				.header("Content-Type", "application/json;charset=utf-8")
 				.header("Accept", "application/json, text/plain, */*")
 				.header("Accept-Language", "en-US,en;q=0.5")
 				.userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0")
-//				.proxy("127.0.0.1", 8080)
-//				.validateTLSCertificates(false)
-				.cookies(res2.cookies())
+				.cookies(cookiesSession)
 				.ignoreContentType(true)
 				.method(Connection.Method.GET)
 				.execute();
 
-		System.err.println(res3.body().toString());
-
-
-		return null;
-    }
-
-    public void dowloadAndParse(String urlStr, Map<String, String> cookies) throws IOException {
-
-
-
-//		// Make a URL to the web page
-//		URL url = new URL(urlStr);
-//
-//		// Get the input stream through URL Connection
-//		URLConnection con = url.openConnection();
-//		InputStream is = con.getInputStream();
-//		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-//
-//		String response = new String();
-//		for (String line; (line = br.readLine()) != null; response += line)
-//			;
-//
-//		System.err.println(response);
-//		new HtmlParser().parse(response);
+		new HtmlParser().parse(res3.body().toString());
 	}
 }
