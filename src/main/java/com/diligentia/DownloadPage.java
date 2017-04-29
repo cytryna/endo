27a34e54444661fe6bc49eca1dcf594c6b7e45cc
radek <radoslaw.wichrowski@gmail.com>
2017-04-29 20:03:47 +0200

    Niepotrzebny try

--- src/main/java/com/diligentia/DownloadPage.java
+++ src/main/java/com/diligentia/DownloadPage.java
@@ -17,67 +17,58 @@ public class DownloadPage {
     public static final String HTTP_URL_HOME = "https://www.endomondo.com/home";
     public static final String HTTP_URL_SESSION = "https://www.endomondo.com/rest/session";
     public static final String HTTP_URL_CHALLENGE = "https://www.endomondo.com/challenges";
-	private SessionLoginRequest sessionLoginRequest;
-	private Map<String, String> cookiesSession;
-	private Map<String, String> cookiesHome;
+    private SessionLoginRequest sessionLoginRequest;
+    private Map<String, String> cookiesSession;
+    private Map<String, String> cookiesHome;
 
-	@PostConstruct
-	public void startBean() throws IOException {
-		Properties prop = new Properties();
-		prop.load(getClass().getClassLoader().getResourceAsStream("app.properties"));
-		sessionLoginRequest = new SessionLoginRequest("radoslaw.wichrowski@gmail.com", prop.get("password").toString(), true);
+    @PostConstruct
+    public void startBean() throws IOException {
+        Properties prop = new Properties();
+        prop.load(getClass().getClassLoader().getResourceAsStream("app.properties"));
+        sessionLoginRequest = new SessionLoginRequest("radoslaw.wichrowski@gmail.com", prop.get("password").toString(), true);
 
-		try {
-			conectAndGetSession();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			dowloadAndParseChallenge(prop.get("challenge").toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        conectAndGetSession();
+        dowloadAndParseChallenge(prop.get("challenge").toString());
+    }
 
     private void conectAndGetSession() throws IOException {
-		Gson gson = new Gson();
-		Connection.Response res = Jsoup
-				.connect(HTTP_URL_HOME)
-				.userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0")
-				.method(Connection.Method.GET)
-				.execute();
-		cookiesHome = res.cookies();
-
-		Connection.Response res2 = Jsoup
-                .connect(HTTP_URL_SESSION)
-				.requestBody(gson.toJson(sessionLoginRequest))
-				.header("X-CSRF-TOKEN", cookiesHome.get("CSRF_TOKEN"))
-				.header("Content-Type", "application/json;charset=utf-8")
-				.header("Accept", "application/json, text/plain, */*")
-				.header("Accept-Language", "en-US,en;q=0.5")
-				.userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0")
-				.cookies(cookiesHome)
-				.ignoreContentType(true)
-				.method(Connection.Method.POST)
+        Gson gson = new Gson();
+        Connection.Response res = Jsoup
+                .connect(HTTP_URL_HOME)
+                .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0")
+                .method(Connection.Method.GET)
                 .execute();
-		cookiesSession = res2.cookies();
+        cookiesHome = res.cookies();
+
+        Connection.Response res2 = Jsoup
+                .connect(HTTP_URL_SESSION)
+                .requestBody(gson.toJson(sessionLoginRequest))
+                .header("X-CSRF-TOKEN", cookiesHome.get("CSRF_TOKEN"))
+                .header("Content-Type", "application/json;charset=utf-8")
+                .header("Accept", "application/json, text/plain, */*")
+                .header("Accept-Language", "en-US,en;q=0.5")
+                .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0")
+                .cookies(cookiesHome)
+                .ignoreContentType(true)
+                .method(Connection.Method.POST)
+                .execute();
+        cookiesSession = res2.cookies();
 
     }
 
     public void dowloadAndParseChallenge(String challengeId) throws IOException {
-		Connection.Response res3 = Jsoup
-				.connect(HTTP_URL_CHALLENGE +"/"+ challengeId)
-				.header("X-CSRF-TOKEN", cookiesHome.get("CSRF_TOKEN"))
-				.header("Content-Type", "application/json;charset=utf-8")
-				.header("Accept", "application/json, text/plain, */*")
-				.header("Accept-Language", "en-US,en;q=0.5")
-				.userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0")
-				.cookies(cookiesSession)
-				.ignoreContentType(true)
-				.method(Connection.Method.GET)
-				.execute();
+        Connection.Response res3 = Jsoup
+                .connect(HTTP_URL_CHALLENGE + "/" + challengeId)
+                .header("X-CSRF-TOKEN", cookiesHome.get("CSRF_TOKEN"))
+                .header("Content-Type", "application/json;charset=utf-8")
+                .header("Accept", "application/json, text/plain, */*")
+                .header("Accept-Language", "en-US,en;q=0.5")
+                .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0")
+                .cookies(cookiesSession)
+                .ignoreContentType(true)
+                .method(Connection.Method.GET)
+                .execute();
 
-		new HtmlParser().parse(res3.body().toString());
-	}
+        new HtmlParser().parse(res3.body().toString());
+    }
 }
