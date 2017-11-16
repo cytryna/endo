b8be15a8c00370e75f2b9ea9c26b4c55e55c5590
rwichrowski <rwichrowski@efigence.com>
2017-11-16 06:42:45 +0100

    zabwy z java 9

---
+++ src/main/java/com/diligentia/sandbox/Java9.java
@@ -0,0 +1,28 @@
+package com.diligentia.sandbox;
+
+import java.util.stream.Stream;
+
+/**
+ * Created by rwichrowski on 10.10.17.
+ */
+public class Java9 {
+    public static void main(String[] args) {
+        new Java9().takeWhileJava9();
+    }
+
+    private void takeWhileJava9() {
+//        Stream.iterate("", s -> s + "s")
+//                .takeWhile(s->s.length() < 3)
+//                .forEach(this::log);
+    }
+
+    private void startTestJava8() {
+        Stream.iterate("a", s -> s + "s")
+                .filter(s->s.length() < 30)
+                .forEach(this::log);
+    }
+
+    private void log(String string) {
+        System.err.println(string);
+    }
+}
